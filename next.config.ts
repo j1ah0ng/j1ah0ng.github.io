fd95053a761f51412750f85888f6fdf388adb15f
Jiahong Long <jiahong888long@gmail.com>
2025-09-03 15:20:18 -0700

    ugh

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'export', // Outputs a Single-Page Application (SPA)
   distDir: 'build', // Changes the build output directory to `build`
+  reactStrictMode: true,
+
 }
  
 export default nextConfig
